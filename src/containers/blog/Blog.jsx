b0ce0c2376ba84dd97e40bb0057689c3eef592db
SaadEssam <SaadEssam059@gmail.com>
2023-01-03 14:37:07 +0200

    Fix br tag

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -7,7 +7,7 @@ const Blog = () => {
   return (
     <div className="gpt3_blog section_padding" id="blog">
       <div className="gpt3_blog-heading">
-        <h1 className="gradient_text">A lot is happening, We are blogging about it.</h1>
+        <h1 className="gradient_text">A lot is happening, <br />We are blogging about it.</h1>
       </div>
       <div className="gpt3_blog-container">
         <div className="gpt3_blog-container_groupA">
